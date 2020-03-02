c3e9584a16c8125059d09420b20269e6f671b147
Raja Shankar Kolluru <raja.shankar@meratransport.com>
2020-03-02 19:32:05 +0400

    Modified the gen-deploy script to do sed substitutions

--- scripts/gen-deploy.sh
+++ scripts/gen-deploy.sh
@@ -1,4 +1,6 @@
 
+
+
 function setenv(){
 	curprog=${1}
 	scripts_folder=${curprog%/*}
@@ -8,9 +10,23 @@ function setenv(){
 	template_folder=$base_folder/template-files/gen-deploy
 	config_folder=$base_folder/config
 	source $config_folder/setenv.sh
+} 
+
+function makeSedScript(){
+	echo "s#__SERVICE_DIR__#$mod#g"
+	echo "s#__URL__#$url#g"
+}
+
+function executeSed(){
+	dest_file=$1
+	template_file=$template_folder/$dest_file
+	full_path_dest_file=$mod/$dest_file
+	sed -f $sedfile $template_file  > $full_path_dest_file
 }
 
 prog=${0##*/}
+sedfile=/tmp/$prog.$$.$RANDOM
+
 if [[ -z $1 ]] || [[ -z $2 ]]
 then
 	echo "Usage: $prog module [URL-for-module]"
@@ -24,6 +40,19 @@ echo "Creating module $mod in folder $scripts_folder with url $url"
 cp -r $template_folder $mod
 cd $mod
 go mod init "$url"
+makeSedScript > $sedfile
+find $template_folder -name "*.go" -print | 
+	while read r
+	do
+		executeSed $r
+	done
+
+find $template_folder -name Dockerfile -print | 
+	while read r
+	do
+		executeSed $r
+	done
+
 cd ..
 
 exit 0
