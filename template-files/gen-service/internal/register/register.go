425244538ed769bff64fb4734552a6222a894bfc
Sumit Pugalia <sumit@cafu.com>
2020-03-05 15:51:53 +0400

    add RAJA's changes for copy bundles and bplus initialization from github

--- template-files/gen-service/internal/register/register.go
+++ template-files/gen-service/internal/register/register.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	api "{{$service.URL}}/api"
 	service "{{$service.URL}}/internal/service"
+	_ "gitlab.intelligentb.com/devops/bplus"                   // initialize BPlus first to make sure
+	// that all BPLUS modules are loaded
 	bplus "gitlab.intelligentb.com/devops/bplus/fw"
 )
 func init(){
