13baeb526a63b1efbfa2de3256db64b7301e2fac
Raja Shankar Kolluru <raja.shankar@meratransport.com>
2020-06-08 06:48:18 +0530

    enhanced the setenv.sh with comments

--- config/setenv.sh
+++ config/setenv.sh
@@ -1,5 +1,10 @@
+# All generated modules will have the prefix shown below.
+# Tweak this if you want it to be something different
 export URLPrefix="github.com/agorago"
+#  The default start error  code if not specified in gen.sh
 export default_start_error_code=100000
+# The URL for WeGO. Change this if you have forked the WeGO repo
 export wegoURL="github.com/agorago/wego"
+# The destination folder where the modules are generated.
 export dest_folder=/tmp
 
